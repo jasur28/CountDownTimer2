ece37b2aa0fe31151fe5f6c88f4c824a899d14ae
jasur28 <yuldashevj@gmail.com>
2023-05-10 07:07:57 +0500

    migration added

---
+++ CountDownTimer2/Migrations/20230510020723_timer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CountDownTimer2.Migrations
+{
+    /// <inheritdoc />
+    public partial class timer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TimerEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TimerStarted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TimerFinished = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimerEntities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimerEntities");
+        }
+    }
+}
